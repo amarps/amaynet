efff3b6b6da1ff824033df32cc9a025562d45202
senjaya <amarpanjis@gmail.com>
2021-09-24 13:53:05 +0700

    move nested class to common

--- include/HTTP/Server.hxx
+++ include/HTTP/Server.hxx
@@ -2,6 +2,7 @@
 #define HTTP_SERVER_HXX
 
 #include "../TCP/TCPListener.hxx"
+#include "common.hxx"
 #include <forward_list>
 #include <sstream>
 #include <string>
@@ -13,121 +14,15 @@ struct sockaddr_storage;
 namespace AMAYNET
 {  
   class HTTPServer : public TCPListener {
-  public:
-    struct HTTPRequest {
-      std::string path;
-      enum Method {
-	INVALID = 0,
-	GET,
-	POST
-      } request_method;
-
-      HTTPRequest(std::string _path, Method _request_method)
-	: path(std::move(_path)),
-	  request_method(_request_method)
-      { }
-
-      bool IsValid() const
-      {
-	return request_method != Method::INVALID;
-      }
-
-    };
-
-    /**
-     * 
-     * message and detail from https://httpstatuses.com
-     */
-    struct status_T {
-      int code;
-      std::string msg;
-      std::string detail;
-  
-      status_T(int _code, const std::string &_msg, const std::string &_detail)
-	:code(_code),
-	 msg(_msg),
-	 detail(_detail) {}
-    };
-
-    enum Status {
-      // Success status
-      OK,
-      CREATED,
-      ACCEPTED,
-
-      // Client Error
-      BAD_REQUEST,
-      UNAUTHORIZED,
-      FORBIDDEN,
-      NOT_FOUND,
-      REQUEST_TIMEOUT,
-
-      // Server Error
-      NOT_IMPLEMENTED,
-      HTTP_VERSION_NOT_SUPPORTED,
-    };
-  
-    /**
-     * @brief
-     * @param port
-     * @return
-     */
+  public:  
     HTTPServer(const std::string &port);
     virtual ~HTTPServer() = default;
-    
-    /**
-     * @brief
-     * @param client
-     * @param status
-     * @param path
-     * @return
-     */
+
+    HTTPRequest GetRequest();
     void SendResponse(const status_T& status, const std::string &content = "");
-
-    HTTPRequest GetRequest() {
-      auto recv_obj = CurrentConnection()->Recv();
-      std::string msg(recv_obj.begin(), recv_obj.end());
-      // check if request msg contain \r\n\r\n that indicate end of request
-      if (msg.find("\r\n\r\n") == std::string::npos) {
-        return HTTPRequest(std::string(), HTTPRequest::INVALID);
-      }
-      
-      auto requestMethod = HTTPRequest::INVALID;
-      auto path = std::string();
-
-      const int num_method_element = 2;
-      std::string str_req_methods[num_method_element] {
-	"GET /",
-	"POST /"
-      };
-
-      for (int mthd_i=0; mthd_i < num_method_element; mthd_i++) {
-	if (msg
-	    .compare(0, str_req_methods[mthd_i].length(),
-		     str_req_methods[mthd_i]) == 0) {
-	  path = msg.substr(str_req_methods[mthd_i].length() - 1);
-	  requestMethod = (HTTPRequest::Method)(mthd_i + 1);
-	  break;
-	}
-      }
-
-      auto end_path_pos = path.find(' ');
-      if (end_path_pos == std::string::npos) { // invalid path. space not found
-	return HTTPRequest(std::string(), HTTPRequest::INVALID);
-      }
-
-      path = path.substr(0, end_path_pos);
-      return HTTPRequest(path, requestMethod);
-    };
-
     void ServeResource(std::string &path, int chunk_size=default_chunk_size);
 
   protected:
-    /**
-     * @brief
-     * @param path
-     * @return
-     */
     static std::string GetContentType(const std::string &path);
     static status_T GetStatus(Status _status);
     
--- include/common.hxx
+++ include/common.hxx
@@ -5,14 +5,64 @@
 
 namespace AMAYNET
 {
-struct URL_T {
-  std::string protocol;
-  std::string host;
-  std::string path;
+  struct URL_T {
+    std::string protocol;
+    std::string host;
+    std::string path;
 
-  URL_T() = default;
-  URL_T(std::string url);
-};
+    URL_T() = default;
+    URL_T(std::string url);
+  };
+
+  struct HTTPRequest {
+    std::string path;
+    enum Method {
+      INVALID = 0,
+      GET,
+      POST
+    } request_method;
+
+    HTTPRequest(std::string _path, Method _request_method)
+      : path(std::move(_path)),
+	request_method(_request_method)
+    { }
+
+    bool IsValid() const
+    {
+      return request_method != Method::INVALID;
+    }
+    
+  };
+
+  /* message and detail from https://httpstatuses.com */
+  struct status_T {
+    int code;
+    std::string msg;
+    std::string detail;
+  
+    status_T(int _code, const std::string &_msg, const std::string &_detail)
+      :code(_code),
+       msg(_msg),
+       detail(_detail) {}
+  };
+
+  enum Status {
+    // Success status
+    OK,
+    CREATED,
+    ACCEPTED,
+
+    // Client Error
+    BAD_REQUEST,
+    UNAUTHORIZED,
+    FORBIDDEN,
+    NOT_FOUND,
+    REQUEST_TIMEOUT,
+
+    // Server Error
+    NOT_IMPLEMENTED,
+    HTTP_VERSION_NOT_SUPPORTED,
+  };
 
 
 } // namespace AMAYNET
